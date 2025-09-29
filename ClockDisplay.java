b976e3e8d9b8de921a40d30c52fb714a53ccd21a
G00275776 <83508525+JimmyAriasops@users.noreply.github.com>
2025-09-29 15:22:30 -0400

    12 hour set up
    Wrote in code for AM/PM transfer, unclear for now as to what i need to do more

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -23,6 +23,7 @@ public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
+    private boolean isAM;
     private String displayString;    // simulates the actual display
     
     /**
@@ -31,9 +32,10 @@ public class ClockDisplay
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
-        updateDisplay();
+        isAM = true;
+        setTime(12, 0, true);
     }
 
     /**
@@ -41,11 +43,11 @@ public class ClockDisplay
      * creates a new clock set at the time specified by the 
      * parameters.
      */
-    public ClockDisplay(int hour, int minute)
+    public ClockDisplay(int hour, int minute, boolean isAM)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
-        setTime(hour, minute);
+        setTime(hour, minute, am);
     }
 
     /**
@@ -57,18 +59,43 @@ public class ClockDisplay
         minutes.increment();
         if(minutes.getValue() == 0) {  // it just rolled over!
             hours.increment();
+            
+            if (hours.getValue() ==0) 
+            {
+                hours.setValue(12);
+                
+                if (isAM == true){
+                    isAM = False;
+                }
+                else {isAM = true;
+              
+            }
         }
         updateDisplay();
     }
 
     /**
      * Set the time of the display to the specified hour and
-     * minute.
+     * minute as well as correct AM/PM time.
      */
-    public void setTime(int hour, int minute)
+    
+    public void setTime(int hour, int minute, boolean am)
     {
-        hours.setValue(hour);
-        minutes.setValue(minute);
+        if (hour == 0)
+        {
+            hour = 12;
+        }
+        else if (hour > 12) 
+        {
+            hour = hour % 12;
+            if (hour == 0)
+            {
+                hour = 12;
+            }
+        }
+        hours.setValue(hour); 
+        minutes.setValue(minute % 60);
+        isAM = am;
         updateDisplay();
     }
 
